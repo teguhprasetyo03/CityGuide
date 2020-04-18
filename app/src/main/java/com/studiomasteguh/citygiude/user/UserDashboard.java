fa179d6a6960525ddb6ca5f56a9e2cb1e104fe3d
teguhprasetyo <teguhprasetyo261211@gmail.com>
2020-04-18 23:13:28 +0700

    membuat dashboard get away

--- app/src/main/java/com/studiomasteguh/citygiude/user/UserDashboard.java
+++ app/src/main/java/com/studiomasteguh/citygiude/user/UserDashboard.java
@@ -3,6 +3,7 @@ package com.studiomasteguh.citygiude.user;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.studiomasteguh.citygiude.R;
 
@@ -12,5 +13,7 @@ public class UserDashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_dashboard);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
     }
 }
